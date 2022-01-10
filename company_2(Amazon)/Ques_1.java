b249c59d1379c4535ec3aa6976a9f80cc49e8111
ritikxsharma <91094131+ritikxsharma@users.noreply.github.com>
2022-01-10 21:55:24 +0530

    Create Ques_1.java

---
+++ company_2(Amazon)/Ques_1.java
@@ -0,0 +1,17 @@
+class Solution {
+   static int maxProfit(int k, int n, int A[]) 
+   {
+       int dp[][]=new int[k+1][n];
+       int max_so=Integer.MIN_VALUE;
+       for(int i=1;i<=k;i++)
+       {
+           max_so=Integer.MIN_VALUE;
+           for(int j=1;j<n;j++)
+           {
+               max_so=Math.max(max_so,dp[i-1][j-1]-A[j-1]);
+               dp[i][j]=Math.max(dp[i][j-1],max_so+A[j]);
+           }
+       }
+       return dp[k][n-1];
+   }
+}
