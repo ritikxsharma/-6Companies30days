32a27e3ec8c126abbab6a913cb591fd3ac55d9db
ritikxsharma <91094131+ritikxsharma@users.noreply.github.com>
2022-01-15 23:10:13 +0530

    Create Ques_8.java

---
+++ company_3(Microsoft)/Ques_8.java
@@ -0,0 +1,52 @@
+//User function Template for Java
+
+ /*
+    Node class is Defined as follows:
+    class Node{
+        int data;
+        Node left;
+        Node right;
+        Node nextRight;
+        Node(int data){
+            this.data = data;
+            left=null;
+            right=null;
+            nextRight = null;
+        }
+    }
+    */
+    
+class Solution
+{
+    //Function to connect nodes at same level.
+    public void connect(Node root)
+    {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(null);
+      
+        while(!queue.isEmpty() && queue.peek() != null)
+        {
+            Node node = queue.poll();
+            
+            if(node.left != null)
+                queue.offer(node.left);
+                
+            if(node.right != null)
+                queue.offer(node.right);
+            while(queue.peek() != null)
+            {
+                node.nextRight = queue.peek();
+                node = queue.poll();
+                
+                if(node.left != null)
+                    queue.offer(node.left);
+                    
+                if(node.right != null)
+                    queue.offer(node.right);
+            }
+            queue.offer(null);
+            queue.poll();
+        }
+    }
+}
