bb2e88f59d00c37e71ddc9464146c2a8eb28f339
ritikxsharma <91094131+ritikxsharma@users.noreply.github.com>
2022-01-15 23:10:38 +0530

    Create Ques_9.java

---
+++ company_3(Microsoft)/Ques_9.java
@@ -0,0 +1,37 @@
+//User function Template for Java
+
+
+/*class Node
+{
+    int data;
+    Node left,right;
+    Node(int d)
+    {
+        data=d;
+        left=right=null;
+    }
+}*/
+
+
+class Tree
+{
+    //Function to count number of subtrees having sum equal to given sum.
+    public static int ans = 0;
+    int countSubtreesWithSumX(Node root, int X)
+    {
+        ans = 0;
+        solve(root , X);
+        return ans;
+    }
+    public static int solve(Node node, int target)
+    {
+        if(node == null)
+            return 0;
+        int sum = node.data;
+        sum += solve(node.left, target);
+        sum += solve(node.right, target);
+        if(sum == target)
+            ans++;
+        return sum;
+    }
+}
