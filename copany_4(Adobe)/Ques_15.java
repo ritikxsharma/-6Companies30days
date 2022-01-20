bbb90e0a45d9c335fe91e73a99b0741321e0414f
ritikxsharma <91094131+ritikxsharma@users.noreply.github.com>
2022-01-20 23:11:30 +0530

    Create Ques_15.java

---
+++ copany_4(Adobe)/Ques_15.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class RecentVersion 
+{
+	public static void main(String[] args) 
+  {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first version name");
+		String version1 = sc.next();
+		String[] version_1 = version1.split("\\.");
+		
+		System.out.println("Enter second version name");
+		String version2 = sc.next();
+		String[] version_2 = version2.split("\\.");
+		
+	  int len = (version_1.length >= version_2.length) ? version_2.length : version_1.length;
+    boolean flag = false;
+		for(int i = 0 ; i < len ; i++)
+		{
+		    if(Integer.parseInt(version_1[i]) > Integer.parseInt(version_2[i]))
+		    {
+		        System.out.println("Version " + version1 + " is more recent");
+		        flag = true;
+		        break;
+		    }
+		        
+		        
+		    else if (Integer.parseInt(version_2[i]) > Integer.parseInt(version_1[i]))
+		    {
+		        System.out.println("Version " + version2 + " is more recent");
+		        flag = true;
+		        break;
+		    }
+		    
+		    else
+		        continue;
+		        
+		}
+		
+		if(!flag)
+		{
+		    if(version1.length() > version2.length())
+		        System.out.println("Version " + version1 + " is more recent");
+		    else
+		        System.out.println("Version " + version2 + " is more recent");
+		}   
+	}
+}
